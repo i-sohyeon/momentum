a91bac6e958364dc782689e912cf618bd0abb430
sohyeon <sohyun9743@naver.com>
2022-05-23 14:07:27 +0900

    update : css
    style 수정
    &units=metric : 기온 단위 수정

--- js/weather.js
+++ js/weather.js
@@ -6,7 +6,7 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     // console.log("you live in", lat, lng);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
    fetch(url).then((response) => response.json())
    .then((data) => {
        city.innerText = data.name;
